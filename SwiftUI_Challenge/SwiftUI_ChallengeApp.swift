a68db9dbe7ed993fda4c82cd1d2d6765488ffcd2
hect <hector.molla@exakthealth.com>
2022-02-07 14:57:34 +0100

    Initial Commit

---
+++ SwiftUI_Challenge/SwiftUI_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChallengeApp.swift
+//  SwiftUI_Challenge
+//
+//  Created by Hector Molla on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
